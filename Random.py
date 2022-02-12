451585ccfdc0fddb9ab46cb201a329a9e14d62d9
Xiddoc <46180972+Xiddoc@users.noreply.github.com>
2022-02-12 19:17:49 +0200

    Fixed Recursion limit issue by using while loop instead

--- Random.py
+++ Random.py
@@ -17,14 +17,17 @@ class Random:
 		"""
 		:return: Returns a random prime number.
 		"""
-		# Get a random number with that amount of bits
-		# [2 ^ (bits - 1)   :to:    2 ^ bits - 1]
-		# For example, for 3 bits:
-		# [0b100            :to:    0b111]
-		random_number: int = randint(2 ** (bits - 1), 2 ** bits - 1)
-		# If it is prime, return it
-		# Otherwise, test a new random number (recursion)
-		return random_number if cls.is_prime(random_number) else cls.get_random_prime(bits=bits)
+		# Until we find a valid random prime
+		while True:
+			# Get a random number with that amount of bits
+			# [2 ^ (bits - 1)   :to:    2 ^ bits - 1]
+			# For example, for 3 bits:
+			# [0b100            :to:    0b111]
+			random_number: int = randint(2 ** (bits - 1), 2 ** bits - 1)
+			# If it is prime
+			if cls.is_prime(random_number):
+				# Return it
+				return random_number
 
 	@classmethod
 	def is_prime(cls, number: int) -> bool:
