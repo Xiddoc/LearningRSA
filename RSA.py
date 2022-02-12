b362ebda0c7c7e48d871509137f8d85213f6296d
Xiddoc <46180972+Xiddoc@users.noreply.github.com>
2022-02-12 18:07:51 +0200

    Partial implementation of RSA algorithm

--- RSA.py
+++ RSA.py
@@ -1,6 +1,9 @@
 """
 RSA class.
 """
+from math import lcm
+
+from Random import Random
 
 import math
 
@@ -12,6 +15,22 @@ class RSA:
 
 	__public_key: int
 	__private_key: int
+
+	def __init__(self, bit_count: int = 1024) -> None:
+		# Get 2 large prime numbers, p and q
+		p: int = Random.get_random_prime(bits=bit_count)
+		q: int = Random.get_random_prime(bits=bit_count)
+		# Calculate n = p * q
+		n: int = p * q
+		# Get totient function of n, which looks like: λ(n)
+		# All of the math is explained on the linked Wikipedia
+		# page, but the takeaway is: λ(n) = lcm(p − 1, q − 1)
+		# FIXME: THIS ONLY WORKS ON Python 3.9
+		n_totient: int = lcm(p - 1, q - 1)
+		# Get value for e (not to be mistaken for Euler's irrational number, which is equal to 2.718...)
+		e: int = 65537  # FIXME: THIS ONLY WORKS IF n_totient is larger than 65537
+		# Calulate d as the modular multiplicative inverse of e modulo λ(n)
+		d: int = pow(e, -1, n_totient)
 
 	def get_public_key(self) -> int:
 		"""
