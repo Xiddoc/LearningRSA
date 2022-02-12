05f77d49fff9ec4c8637b6b48d9386de78feb81b
Xiddoc <46180972+Xiddoc@users.noreply.github.com>
2022-02-12 19:19:04 +0200

    Improved script verbosity

--- main.py
+++ main.py
@@ -13,8 +13,12 @@ print(f"Private key: {rsa.get_private_key()}")
 # Print public key
 print(f"Public key: {rsa.get_public_key()}")
 
+# Print plaintext
+plaintext_message = "This is a message!"
+print(f"Plaintext message: {plaintext_message}")
+
 # Use public key to encrypt a message
-encrypted_message = RSA.encrypt("testing", rsa.get_public_key())
+encrypted_message = RSA.encrypt(plaintext_message, rsa.get_public_key())
 print(f"Encrypted message: {encrypted_message}")
 
 # Decrypt
