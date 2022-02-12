bb7d45518ddc23b5baa705f95426da3325cd5feb
Xiddoc <46180972+Xiddoc@users.noreply.github.com>
2022-02-12 19:10:12 +0200

    Example for RSA usage

--- main.py
+++ main.py
@@ -8,8 +8,14 @@ from RSA import RSA
 rsa = RSA()
 
 # Print out private key (secret!!!)
-print(rsa.get_private_key())
+print(f"Private key: {rsa.get_private_key()}")
 
 # Print public key
-print(rsa.get_public_key())
+print(f"Public key: {rsa.get_public_key()}")
 
+# Use public key to encrypt a message
+encrypted_message = RSA.encrypt("testing", rsa.get_public_key())
+print(f"Encrypted message: {encrypted_message}")
+
+# Decrypt
+print(f"Decrypted message: {rsa.decrypt(encrypted_message)}")
