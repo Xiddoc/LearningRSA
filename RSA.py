ea239e28085ee05179be0b073586fe0777ad6a1c
Xiddoc <46180972+Xiddoc@users.noreply.github.com>
2022-04-03 22:30:52 +0300

    Improved docs and exposed simpler API

--- RSA.py
+++ RSA.py
@@ -1,100 +1,121 @@
 """
 RSA class.
 """
-from math import ceil
+from json import loads, dumps
 from typing import Tuple
-import json
 
 from MathUtils import MathUtils
 
 
+# noinspection PyUnusedFunction
 class RSA:
-	"""
+    """
 	RSA class.
 	For a new public and private key, make a new instance.
 	"""
 
-	__public_key: Tuple[int, int]
-	__private_key: int
+    __public_key: Tuple[int, int]
+    __private_key: int
 
-	def __init__(self, bit_count: int = 1024) -> None:
-		# Get 2 large prime numbers, p and q
-		p: int = MathUtils.get_random_prime(bits=bit_count)
-		q: int = MathUtils.get_random_prime(bits=bit_count)
-		# Calculate n = p * q
-		n: int = p * q
-		# Get totient function of n, which looks like: λ(n)
-		# All of the math is explained on the linked Wikipedia
-		# page, but the takeaway is: λ(n) = lcm(p − 1, q − 1)
-		# FIXME: THIS ONLY WORKS ON Python 3.9
-		n_totient: int = MathUtils.lcm(p - 1, q - 1)
-		# Get value for e (not to be mistaken for Euler's irrational number, which is equal to 2.718...)
-		e: int = 65537  # FIXME: THIS ONLY WORKS IF n_totient is larger than 65537
-		# Calulate d as the modular multiplicative inverse of e modulo λ(n)
-		d: int = pow(e, -1, n_totient)
+    def __init__(self, bit_count: int = 1024) -> None:
+        # Get 2 large prime numbers, p and q
+        p: int = MathUtils.get_random_prime(bits=bit_count)
+        q: int = MathUtils.get_random_prime(bits=bit_count)
+        # Calculate n = p * q
+        n: int = p * q
+        # Get totient function of n, which looks like: λ(n)
+        # All of the math is explained on the linked Wikipedia
+        # page, but the takeaway is: λ(n) = lcm(p − 1, q − 1)
+        n_totient: int = MathUtils.lcm(p - 1, q - 1)
+        # Get value for e (not to be mistaken for Euler's irrational number, which is equal to 2.718...)
+        e: int = 65537  # FIXME: THIS ONLY WORKS IF n_totient is larger than 65537
+        # Calculate d as the modular multiplicative inverse of e modulo λ(n)
+        d: int = pow(e, -1, n_totient)
 
-		# Let's assign the keys to the class variables
-		self.__public_key = (e, n)  # (Public Exponent, Modulus)
-		self.__private_key = d  # Private Exponent
+        # Let's assign the keys to the class variables
+        self.__public_key = (e, n)  # (Public Exponent, Modulus)
+        self.__private_key = d  # Private Exponent
 
-		# Delete unnecessary variables from memory
-		del p, q, n_totient
+        # Delete unnecessary variables from memory
+        del p, q, n_totient
 
-	@classmethod
-	def encrypt(cls, message: str, public_key: Tuple[int, int]) -> int:
+    @classmethod
+    def encrypt(cls, message: str, public_key: Tuple[int, int]) -> int:
+        """
+		Wrapper function for encrypt_from_bytes.
 		"""
-		Encrypt a string using an RSA public key.
+        return cls.encrypt_from_bytes(message.encode('utf-8'), public_key)
+
+    @classmethod
+    def encrypt_from_bytes(cls, message: bytes, public_key: Tuple[int, int]) -> int:
+        """
+		Encrypt a bytes object using an RSA public key.
 
 		:param message: The string to encrypt.
 		:param public_key: The public key to encrypt with.
 		:return: The integer representation of the encrypted string.
 		"""
-		# Convert message to an integer
-		# message ^ public_exponent ( % public_modulo )
-		return pow(cls.__string_to_int(message), public_key[0], public_key[1])
+        # Convert message to an integer
+        # message ^ public_exponent ( % public_modulo )
+        return pow(cls.__bytes_to_int(message), public_key[0], public_key[1])
 
-	def decrypt(self, encrypted_message: int) -> str:
+    def decrypt(self, encrypted_message: int) -> str:
+        """
+		Wrapper for decrypt_to_bytes.
 		"""
+        return self.decrypt_to_bytes(encrypted_message).decode('utf-8')
+
+    def decrypt_to_bytes(self, encrypted_message: int) -> bytes:
+        """
 		Decrypt an RSA encrypted string.
 
 		:param encrypted_message: The message to decrypt.
 		:return: The cleartext string.
 		"""
-		# message ^ private_exponent ( % public_modulo )
-		# Then convert the message to a string
-		return self.__int_to_string(pow(encrypted_message, self.__private_key, self.__public_key[1]))
+        # message ^ private_exponent ( % public_modulo )
+        # Then convert the message to a string
+        return self.__int_to_bytes(pow(encrypted_message, self.__private_key, self.__public_key[1]))
 
-	def get_public_key(self) -> Tuple[int, int]:
-		"""
+    def get_public_key(self) -> Tuple[int, int]:
+        """
 		:return: This instance's public key.
 		"""
-		return self.__public_key
+        return self.__public_key
 
-	def get_private_key(self) -> int:
-		"""
-		:return: This instance's private key.
-		"""
-		return self.__private_key
+    def get_serialized_pub_key(self) -> bytes:
+        """
+		Serialize the public key to a bytes object.
+        """
+        # Put values in dict
+        # Then serialize to JSON
+        return dumps({
+	        "e": self.__public_key[0],
+	        "m": self.__public_key[1]
+        }).encode("utf-8")
 
-	@staticmethod
-	def __string_to_int(data: str) -> int:
+    @staticmethod
+    def __bytes_to_int(data: bytes) -> int:
+        """
+		:return: Bytes to Integer
 		"""
-		:return: String to Integer
-		"""
-		return int.from_bytes(data.encode(), byteorder='big')
+        return int(data.hex(), 16)
 
-	@staticmethod
-	def __int_to_string(data: int) -> str:
+    @staticmethod
+    def __int_to_bytes(data: int) -> bytes:
+        """
+		:return: Integer to Bytes
 		"""
-		:return: Integer to String
-		"""
-		return data.to_bytes(ceil(data.bit_length() / 8), byteorder='big').decode()
+        return bytes.fromhex(hex(data)[2:])
 
-	@staticmethod
-	def serialize_pub_key(key: Tuple[int, int]) -> bytes:
-		return json.dumps({"e": key[0], "m": key[1]}).encode("utf-8")
+    @staticmethod
+    def deserialize_pub_key(data: bytes) -> Tuple[int, int]:
+        """
+		Takes a byte object and deserializes it to a public key.
 
-	@staticmethod
-	def deserialize_pub_key(data: bytes) -> Tuple[int, int]:
-		pub_dict = json.loads(data.decode("utf-8"))
-		return pub_dict["e"], pub_dict["m"]
+        :param data: The serialized public key.
+        :return: The public key.
+        """
+        # Parse JSON
+        pub_dict = loads(data.decode("utf-8"))
+        # Pull out values
+        return pub_dict["e"], pub_dict["m"]
--- example.py
+++ example.py
@@ -7,18 +7,15 @@ from RSA import RSA
 # Make instance of RSA class (this generates private / public key pair)
 rsa = RSA()
 
-# Print out private key (secret!!!)
-print(f"\n\nPrivate key: {rsa.get_private_key()}")
-
 # Print public key
 print(f"\n\nPublic key: {rsa.get_public_key()}")
 
 # Print plaintext
 plaintext_message = 'RSA (Rivest–Shamir–Adleman) is a public-key cryptosystem that is widely used for secure data ' \
                     'transmission. It is also one of the oldest. The acronym "RSA" comes from the surnames of Ron ' \
-                    'Rivest, Adi Shamir and Leonard Adleman, who publicly described the algorithm in 1977. An ' \
-                    'equivalent system was developed secretly in 1973 at GCHQ (the British signals intelligence ' \
-                    'agency) by the English mathematician Clifford Cocks. That system was declassified in 1997.'
+                    # 'Rivest, Adi Shamir and Leonard Adleman, who publicly described the algorithm in 1977. An ' \
+                    # 'equivalent system was developed secretly in 1973 at GCHQ (the British signals intelligence ' \
+                    # 'agency) by the English mathematician Clifford Cocks. That system was declassified in 1997.'
 
 print(f"\n\nPlaintext message: {plaintext_message}")
 
